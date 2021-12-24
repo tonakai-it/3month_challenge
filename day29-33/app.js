cdb28524c937ecf1773b32129c7eb7303635fd09
tonakai-it <akifujitonakai@icloud.com>
2021-12-24 18:58:53 +0900

    day30

---
+++ day29-33/app.js
@@ -0,0 +1,18 @@
+let age = 26;
+let greetingText = "Hi I'm Max!";
+alert(greetingText);
+greetingText = 'Hi, I am really Max';
+alert(greetingText);
+alert(age);
+
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = {
+    title: "Developer",
+    place: "tokyo",
+    salary: 200000
+}
+
+let jobTitle = job.title
+
+alert(hobbies[2])
