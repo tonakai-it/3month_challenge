ce1792da96995d7e2e4cea5442cb57cc653d7fd7
tonakai-it <akifujitonakai@icloud.com>
2021-12-26 10:33:20 +0900

    day32

--- day29-33/app.js
+++ day29-33/app.js
@@ -25,8 +25,20 @@ function calculateAdultYears(userAge) {
 }
 
 totalAdultYears = calculateAdultYears(age);
-alert(totalAdultYears);
+// alert(totalAdultYears);
+console.log(totalAdultYears);
 
 age = 45;
 totalAdultYears = calculateAdultYears(age);
-alert(totalAdultYears);
+// alert(totalAdultYears);
+console.log(totalAdultYears)
+
+let person = {
+    name: 'Max', // property
+    greet(){ // method
+        // alert('Hello!');
+        console.log('Hello!')
+    }
+};
+
+person.greet();
---
+++ day29-33/operations.js
@@ -0,0 +1,22 @@
+// math operations
+
+console.log(10 + 4)
+console.log(10 - 4)
+console.log(10 * 4)
+console.log(10 / 4)
+console.log(10 % 4)
+console.log(10 % 3)
+
+console.log(10 + 3 - 5 * 10); // 37
+
+let result = 10 * 4;
+result += 1; // result = result + 1
+result ++;
+console.log(result)
+
+console.log("fuga"+"hoge"); // fugahoge
+console.log("FUga"- "ga") // NaN
+
+let userName = 'Max';
+console.log(userName.length);
+console.log(userName.toUpperCase())
