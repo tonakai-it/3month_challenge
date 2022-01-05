57cd9bc6a52c630b8f81e01c5118aa3b967b1282
tonakai-it <akifujitonakai@icloud.com>
2022-01-05 21:17:13 +0900

    day42

--- day40-44/scripts/config.js
+++ day40-44/scripts/config.js
@@ -1,9 +1,33 @@
-function openPlayerConfig(){
+function openPlayerConfig(event){
+    editedPlayer = +event.target.dataset.playerid;
     playerConfigOverlayElement.style.display = 'block';
-    backdropElement.style.display = 'block'
+    backdropElement.style.display = 'block';
 }
 
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none'
+    backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputelement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = "";
+}
+
+function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim();
+    // console.log(enteredPlayername);
+
+    if (!enteredPlayername) {
+        event.target.firstElementChild.classList.add('error');
+        errorsOutputelement.textContent = 'Please enter a valid name!';
+        return;
+    }
+
+    const updatedPlayerDataElement = document.getElementById(`player-${editedPlayer}-data`);
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+    players[editedPlayer - 1].name = enteredPlayername;
+
+    closePlayerConfig();
 }
