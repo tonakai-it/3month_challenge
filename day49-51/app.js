572ac3e45ee8a3f86f15ff53a22d94efc1a84562
tonakai-it <akifujitonakai@icloud.com>
2022-01-14 21:23:17 +0900

    day50

--- day49-51/app.js
+++ day49-51/app.js
@@ -1,33 +1,50 @@
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
 
 const express = require('express');
-const app = express()
-app.use(express.urlencoded(extended=false))
-app.use(express.static(path.resolve(__dirname, "views")))
+const app = express();
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
 
 app.get("/", (req,res) =>{
-    res.sendFile(path.resolve(__dirname, 'views','index.html'))
+    res.render('index')
 })
 
 app.get("/restaurants", (req, res)=>{
-    const htmlFilePath = path.resolve(__dirname, 'views', 'restaurants.html')
-    res.sendFile(htmlFilePath)
+    const filePath = path.join(__dirname, 'data', 'restaurants.json')
+
+    const fileData = fs.readFileSync(filePath)
+    const storedRestaurants = JSON.parse(fileData);
+    res.render('restaurants', { numberOfRestaurants:storedRestaurants.length, restaurants:storedRestaurants  })
 })
 
 app.get("/recommend", (req, res)=>{
-    const htmlFilePath = path.resolve(__dirname, 'views', 'recommend.html')
-    res.sendFile(htmlFilePath)
+    res.render('recommend')
+})
+
+app.post("/recommend", (req,res) =>{
+    const restaurant = req.body;
+    const filePath = path.join(__dirname, 'data', 'restaurants.json')
+
+    const fileData = fs.readFileSync(filePath)
+    const storedRestaurants = JSON.parse(fileData);
+    storedRestaurants.push(restaurant)
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+    res.redirect('/confirm');
 })
 
 app.get("/confirm", (req, res)=>{
-    const htmlFilePath = path.resolve(__dirname, 'views', 'confirm.html')
-    res.sendFile(htmlFilePath)
+    res.render('confirm')
 })
 
 app.get("/about", (req, res)=>{
-    const htmlFilePath = path.resolve(__dirname, 'views', 'about.html')
-    res.sendFile(htmlFilePath)
+    res.render('about')
 })
+
 
 app.listen(3000, ()=>{console.log("http://localhost:3000")})
